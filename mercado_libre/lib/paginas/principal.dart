657f44da65a5ace52aeab8c37b24c2e0e28bf4b0
raulbau <raulb@111btec.com>
2020-11-12 02:39:55 -0600

    se agrego el carrusel de imagenes automatico

--- mercado_libre/lib/paginas/principal.dart
+++ mercado_libre/lib/paginas/principal.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:mercado_libre/main.dart';
 import 'package:mercado_libre/paginas/login.dart';
 import 'package:mercado_libre/widgets/appBar.dart';
+import 'package:mercado_libre/widgets/iconoCategoria.dart';
+import 'package:mercado_libre/widgets/tarjetaSlider.dart';
 
 class Principal extends StatefulWidget {
   static String routeName = 'principal';
@@ -10,6 +14,38 @@ class Principal extends StatefulWidget {
 }
 
 class _PrincipalState extends State<Principal> {
+  PageController pageController;
+
+  Timer timer;
+
+  var paginasSlider = [
+    TarjetaSlider(imagen: 'assets/1.jpg'),
+    TarjetaSlider(imagen: 'assets/2.jpg'),
+    TarjetaSlider(imagen: 'assets/3.jpg'),
+    TarjetaSlider(imagen: 'assets/4.jpg'),
+    TarjetaSlider(imagen: 'assets/5.jpg'),
+  ];
+
+  @override
+  void initState() {
+    pageController = PageController(
+      viewportFraction: .9,
+      initialPage: 5,
+    );
+    timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      pageController.nextPage(
+          duration: Duration(milliseconds: 600), curve: Curves.ease);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +75,40 @@ class _PrincipalState extends State<Principal> {
             ),
           ],
         ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [
+                colores['amarillo'],
+                Theme.of(context).scaffoldBackgroundColor
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            )),
+            height: 170,
+            width: double.infinity,
+            child: PageView.builder(
+              itemBuilder: (context, index) {
+                return paginasSlider[index % paginasSlider.length];
+              },
+              controller: pageController,
+              physics: BouncingScrollPhysics(),
+              pageSnapping: true,
+              onPageChanged: (val) {
+                // pageController.
+              },
+            ),
+          ),
+          IconoCategoria(
+            icono: Icons.shop,
+            colorFondo: Colors.white,
+            diametro: 55,
+            nombre: "Compras Compras Compras Compras ",
+          ),
+        ],
       ),
     );
   }
